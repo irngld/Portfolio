cabd8ec56f86ed5d390768392fcc27829336d916
irngld <iaingould1@gmail.com>
2021-03-23 21:15:53 -0400

    updated background, header, navbar

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// fullName transition
+$(".animate-slide-right").velocity("transition.slideRightBigIn", { display: null, delay: 500, stagger: 140, drag: true} );
+
+// and Career 
+$(".animate-slide-up").velocity("transition.slideRightBigIn", { display: null, delay: 2000, stagger: 140, drag: true} );
