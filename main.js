fdc865230e8cb52205f9657f6fb1de43eafeb5dc
irngld <iaingould1@gmail.com>
2021-03-25 23:39:42 -0400

    JS - JQuery (un)hide projects and about. CSS .hidden added.

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ $(".animate-slide-right").velocity("transition.slideRightBigIn", { display: null
 
 // and Career 
 $(".animate-slide-up").velocity("transition.slideRightBigIn", { display: null, delay: 2000, stagger: 140, drag: true} );
+
+$(".lnk-technologies").click(() => {
+    if($("#container-technologies").hasClass("hidden")){
+        $(".tabs").addClass("hidden");
+        $("#container-technologies").removeClass("hidden");
+    }
+})
+
+
+
+$(".lnk-projects").click(() => {
+    if($("#container-projects").hasClass("hidden")){
+        $('.tabs').addClass("hidden");
+        $("#container-projects").removeClass('hidden');
+    }
+})
+
+$(".lnk-about").click(() => {
+    if($("#container-about").hasClass("hidden")){
+        $('.tabs').addClass("hidden");
+        $("#container-about").removeClass('hidden');
+    }
+})
