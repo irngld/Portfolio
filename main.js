062fcaeb14675d783dc1848a2e43857ade3b4376
irngld <iaingould1@gmail.com>
2021-04-03 00:14:57 -0400

    fixed CSS contact footer & links, added formspree.io (JS working)

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ $(".animate-slide-right").velocity("transition.slideRightBigIn", { display: null
 // and Career 
 $(".animate-slide-up").velocity("transition.slideRightBigIn", { display: null, delay: 2000, stagger: 140, drag: true} );
 
+
+// Navbar
 $(".lnk-technologies").click(() => {
     if($("#container-technologies").hasClass("hidden")){
         $(".tabs").addClass("hidden");
         $("#container-technologies").removeClass("hidden");
     }
 })
-
 
 
 $(".lnk-projects").click(() => {
@@ -27,3 +28,37 @@ $(".lnk-about").click(() => {
         $("#container-about").removeClass('hidden');
     }
 })
+
+
+/* paste this line in verbatim */
+window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+
+/* customize formbutton below*/     
+formbutton("create", {
+  action: "https://formspree.io/f/mgerevlg",
+  title: "How can we help?",
+  fields: [
+    { 
+      type: "email", 
+      label: "Email:", 
+      name: "email",
+      required: true,
+      placeholder: "your@email.com"
+    },
+    {
+      type: "textarea",
+      label: "Message:",
+      name: "message",
+      placeholder: "What's on your mind?",
+    },
+    { type: "submit" }      
+  ],
+  styles: {
+    title: {
+      backgroundColor: "gray"
+    },
+    button: {
+      backgroundColor: "gray"
+    }
+  }
+});
